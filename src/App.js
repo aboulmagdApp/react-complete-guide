5d58e68cd02ddb693d55f1eaa0a082d15c892982
Mohamed Aboulmagd <aboulmagd@live.com>
2020-07-06 15:58:39 +0300

    Handling Events with Methods

--- src/App.js
+++ src/App.js
@@ -4,11 +4,26 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'aboulmagd', age: 37},
+      {name: 'Max', age: 28},
+      {name: 'stehanie', age: 22}
+    ]
+  }
+
+  switchNameHandeler = () =>{
+    console.log('Was clicked');
+  }
+
   render(){
     return (
       <div className="App">
         <h1>Hi I'm a react app</h1>
-        <Person name="boulmagd" age="37"></Person>
+        <button onClick={this.switchNameHandeler}>switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I am happy</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
       </div>
     );
   }
